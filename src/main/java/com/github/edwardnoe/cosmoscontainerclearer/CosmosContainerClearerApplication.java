d8db820b3ec3570b66d9b5672984a1577987c6db
Edward Noe <eln4jtt@virginia.edu>
2021-02-15 22:13:11 -0500

    Add template Spring Boot app

---
+++ src/main/java/com/github/edwardnoe/cosmoscontainerclearer/CosmosContainerClearerApplication.java
@@ -0,0 +1,13 @@
+package com.github.edwardnoe.cosmoscontainerclearer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CosmosContainerClearerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CosmosContainerClearerApplication.class, args);
+	}
+
+}
